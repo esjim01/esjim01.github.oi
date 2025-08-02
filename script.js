4d8ddc9a1a85c088b107f05160e65b7877304e74
esjim01 <eduar.jimenez45164@ucaldas.edu.co>
2025-08-02 11:56:37 -0500

    100%

---
+++ script.js
@@ -0,0 +1,22 @@
+function downloadPDF() {
+    const element = document.querySelector('.container-lg');
+
+    const opt = {
+        margin: [2, 5, 1 , 5],  // [arriba, izquierda, abajo, derecha] en mm
+        filename: 'Curriculum_vitae_Stiven_jimenez.pdf', // Nombre del archivo
+        image: { type: 'jpeg', quality: 0.98 }, // Configuración de la imagen
+        html2canvas: { // Configuración de html2canvas
+            scale: 2,// Escala de la imagen
+            useCORS: true,// Permitir CORS para imágenes
+            allowTaint: false,// No permitir contaminación de imágenes
+            scrollY: 0 // Evitar el desplazamiento vertical
+        },
+        jsPDF: {
+            unit: 'mm', // Unidad de medida
+            format: 'a4', // Formato del PDF
+            orientation: 'portrait'// Orientación del PDF
+        }
+    };
+
+    html2pdf().set(opt).from(element).save(); // Iniciar la descarga del PDF
+}
